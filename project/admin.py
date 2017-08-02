6fc54f504ab5e8f9e12ebb1383cd5fc7bca0edea
katsuya0719 <katsuya0719@gmail.com>
2017-08-03 00:23:13 +0900

    admin画面に行こうとすると、'html' object has no attribute 'project'っていうエラーが出る

--- project/admin.py
+++ project/admin.py
@@ -3,3 +3,7 @@ from .import models
 
 # Register your models here.
 admin.site.register(models.html)
+admin.site.register(models.location)
+admin.site.register(models.program)
+admin.site.register(models.project)
+admin.site.register(models.scheme)
